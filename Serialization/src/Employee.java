1e3e9fa32225aa066acbe7bb15cb92c37d5bc36b
unknown <kasalanatipavan888@gmail.com>
2020-01-21 04:29:36 +0530

    first commit

---
+++ Serialization/src/Employee.java
@@ -0,0 +1,9 @@
+import java.io.Serializable;  
+public class Employee implements Serializable{  
+	 int id;  
+	 String name;  
+	 public Employee(int id, String name) {  
+	  this.id = id;  
+	  this.name = name;  
+	 }  
+	}  
